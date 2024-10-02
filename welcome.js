c2d80380c1cd34c1f2bbbd1f18e48fb7a0558584
Nickettyang <nickettyang45@gmail.com>
2024-10-02 08:29:38 +0300

    last commit

--- welcome.js
+++ welcome.js
@@ -1,14 +1,16 @@
-// welcome.js
-const fs = require('fs');
+const fs = require("fs");
 
 // Create welcome.txt
-fs.writeFileSync('welcome.txt', 'Hello Node');
+fs.writeFileSync("welcome.txt", "Hello Node");
+
+// Create hello.txt if it doesn't exist (optional)
+fs.writeFileSync("hello.txt", "Hello from hello.txt");
 
 // Read from hello.txt
-fs.readFile('hello.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading file:', err);
-        return;
-    }
-    console.log(data);
+fs.readFile("hello.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  console.log(data); // This will output the content of hello.txt
 });
